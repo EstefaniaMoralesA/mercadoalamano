3bfb8ba50586de6ad983e3ddfd3d9b13227888a9
EstefaniaMoralesA <estefi_ma94@hotmail.com>
2017-05-31 17:03:42 -0500

    created project on xcode

---
+++ MercadoALaMano/MercadoALaMano/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MercadoALaMano
+//
+//  Created by Estefanía Morales Abud on 31/05/17.
+//  Copyright © 2017 com.gueros. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
